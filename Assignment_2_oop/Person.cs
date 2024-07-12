9628b78eb50b1d86a76d8bfbd2b4e094ed188064
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-12 13:14:53 +0300

    Q1-Part02

---
+++ Assignment_2_oop/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_oop
+{
+    internal struct Person
+    {
+
+        #region Attributes
+        public string Name { get; set; }
+        public int Age { get; set; } 
+        #endregion
+
+        // Method to display person details
+        public override string ToString()
+            {
+                return $"Name is : {Name}, Age is : {Age}";
+            }
+        
+    }
+}
